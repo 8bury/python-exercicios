7c85b6cdbcaee9eae663dc22c733dac2ad60c8ce
8bury <pedro.h@aluno.uepb.edu.br>
2023-08-25 13:23:37 -0300

    modified:   unidade_1/ex1-40.py

--- unidade_1/ex1-40.py
+++ unidade_1/ex1-40.py
@@ -4,60 +4,72 @@ operacao = input("Digite a operação (+, -, x ou /): ")
 if operacao == '+':
     resultado = num1 + num2
     print(f'{num1} + {num2} = {resultado}')
-    if resultado % 2 == 0:
-        print('O número é par')
+    if resultado == 0:
+        print('O número é neutro')
     else:
-        print('O número é ímpar')
-    if resultado > 0:
-        print('O número é positivo')
-    else:
-        print('O número é negativo')
-    if resultado % 1 == 0:
-        print('O número é inteiro')
-    else:
-        print('O número é decimal')
+        if resultado % 2 == 0:
+            print('O número é par')
+        else:
+            print('O número é ímpar')
+        if resultado > 0:
+            print('O número é positivo')
+        else:
+            print('O número é negativo')
+        if resultado % 1 == 0:
+            print('O número é inteiro')
+        else:
+            print('O número é decimal')
 elif operacao == '-':
     resultado = num1 - num2
     print(f'{num1} - {num2} = {resultado}')
-    if resultado % 2 == 0:
-        print('O número é par')
+    if resultado == 0:
+        print('O número é neutro')
     else:
-        print('O número é ímpar')
-    if resultado > 0:
-        print('O número é positivo')
-    else:
-        print('O número é negativo')
-    if resultado % 1 == 0:
-        print('O número é inteiro')
-    else:
-        print('O número é decimal')
+        if resultado % 2 == 0:
+            print('O número é par')
+        else:
+            print('O número é ímpar')
+        if resultado > 0:
+            print('O número é positivo')
+        else:
+            print('O número é negativo')
+        if resultado % 1 == 0:
+            print('O número é inteiro')
+        else:
+            print('O número é decimal')
 elif operacao == 'x':
     resultado = num1 * num2
     print(f'{num1} x {num2} = {resultado}')
-    if resultado % 2 == 0:
-        print('O número é par')
+    if resultado == 0:
+        print('O número é neutro')
     else:
-        print('O número é ímpar')
-    if resultado > 0:
-        print('O número é positivo')
-    else:
-        print('O número é negativo')
-    if resultado % 1 == 0:
-        print('O número é inteiro')
-    else:
-        print('O número é decimal')
+        if resultado % 2 == 0:
+            print('O número é par')
+        else:
+            print('O número é ímpar')
+        if resultado > 0:
+            print('O número é positivo')
+        else:
+            print('O número é negativo')
+        if resultado % 1 == 0:
+            print('O número é inteiro')
+        else:
+            print('O número é decimal')
 elif operacao == '/':
     resultado = num1 / num2
     print(f'{num1} / {num2} = {resultado}')
-    if resultado % 2 == 0:
-        print('O número é par')
+    if resultado == 0:
+        print('O número é neutro')
     else:
-        print('O número é ímpar')
-    if resultado > 0:
-        print('O número é positivo')
-    else:
-        print('O número é negativo')
-    if resultado % 1 == 0:
-        print('O número é inteiro')
-    else:
-        print('O número é decimal')
+        if resultado % 2 == 0:
+            print('O número é par')
+        else:
+            print('O número é ímpar')
+        if resultado > 0:
+            print('O número é positivo')
+        else:
+            print('O número é negativo')
+        if resultado % 1 == 0:
+            print('O número é inteiro')
+        else:
+            print('O número é decimal')
